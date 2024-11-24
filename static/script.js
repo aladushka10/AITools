bfd1ae87c2cac02997c0e5a7c84158d0f9fa4249
Aladko Nastya <aladko05@mail.ru>
2024-11-24 23:45:22 +0300

    scroll

--- static/script.js
+++ static/script.js
@@ -1,14 +1,15 @@
 // static/script.js
 async function sendG4FRequest(input) {
-    const response = await fetch('/api/g4f_query', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ prompt: input })
-    });
-    
-    const data = await response.json();
-    console.log(data);
-    document.getElementById('response').textContent = data.response;
+  const response = await fetch("/api/g4f_query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ prompt: input }),
+  })
+
+  const data = await response.json()
+  console.log(data)
+  const responseText = document.getElementById("response").textContent
+  document.getElementById("response").textContent = data.response
 }
