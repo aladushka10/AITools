ae5b3855481cdada39f61f5902088d23a66a7608
Aladko Nastya <aladko05@mail.ru>
2025-04-13 20:35:45 +0300

    code mermaid

--- static/js/script.js
+++ static/js/script.js
@@ -65,6 +65,15 @@ document.addEventListener("DOMContentLoaded", () => {
         add.innerHTML = marked.parse(toInsert.trim())
         document.querySelector(".response:last-of-type").append(add)
       }
+    } else if (data.model === "diagram") {
+      const add = document.createElement("div")
+      const img = document.createElement("div")
+      img.innerHTML = data.response
+      img.className = "mermaid"
+      img.style.width = "400px"
+      img.style.height = "400px"
+      add.append(img)
+      document.querySelector(".response:last-of-type").append(add)
     } else if (data.model === "image") {
       const add = document.createElement("div")
       const img = document.createElement("img")
